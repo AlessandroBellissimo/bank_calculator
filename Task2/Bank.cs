f62a10a16c222fe639bfa040c81a0017fe1087b8
Alessandro <gizz.bellissimo@yandex.ru>
2017-02-13 17:13:13 +0300

    Проект BankCalculator добавлен в репозиторий

---
+++ Task2/Bank.cs
@@ -0,0 +1,23 @@
+﻿namespace Task2
+{
+    public class Bank
+    {
+        public string Name { get; private set; }            // название банка
+        public double InterestRate { get; private set; }    // процентная ставка
+
+        public Bank(string name, double interestRate)
+        {
+            Name = name;
+            InterestRate = interestRate;
+        }
+
+        /// <summary>
+        /// Вычислить доход:
+        /// </summary>
+        public double GetIncome(double p, int t, int K)
+        {
+            double S = p * (1 + (InterestRate * t) / (K * 100));
+            return S;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.FindOptimalBank();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Поиск оптимального банка
+        /// </summary>
+        private void FindOptimalBank()
+        {
+            List<Bank> banks = new List<Bank>
+            {
+                new Bank("A",0.1),
+                new Bank("B",0.3),
+                new Bank("C",0.9),
+                new Bank("D",0.7),
+                new Bank("E",0.2)
+            };
+
+            Console.WriteLine("Список банков:");
+            foreach (Bank b in banks)
+            {
+                Console.WriteLine("{0}", b.Name);
+            }
+
+            int K;
+            if (DateTime.IsLeapYear(DateTime.Now.Year))
+                K = 366;
+            else K = 365;
+            Console.WriteLine("Введите количество дней начисления процентов:");
+            int t = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите сумму денег на вкладе:");
+            double p = int.Parse(Console.ReadLine());
+            
+            banks.Sort(delegate (Bank x, Bank y)
+            {
+                return x.GetIncome(p, t, K).CompareTo(y.GetIncome(p, t, K));
+            });
+
+            // ВНИМАНИЕ! Запись delegate (SomeType x, SomeType y)
+            // {
+            //     return x.GetIncome(p, t, K).CompareTo(y.GetIncome(p, t, K));
+            // });
+            // означает: вызывается метод-делегат, 
+            // который принимает два аргумента
+            // и возвращает сравнение этих аргументов
+            // по определенному признаку
+
+            Console.WriteLine("Оптимальный вариант:");
+
+            Console.WriteLine("Банк: {0}, доход: {1:F2}", 
+                banks[banks.Count - 1].Name, banks[banks.Count - 1].GetIncome(p, t, K));
+        }
+    }
+}
